8aecbde33cca5d6675a2ce92b3602806367f14eb
Luis Lucas <luis@handcom.com.br>
2022-05-21 20:46:34 -0300

    Create Modal Cadastro

--- src/components/ModalLogin/index.vue
+++ src/components/ModalLogin/index.vue
@@ -4,7 +4,10 @@
       Entre na sua conta
     </h1>
 
-    <button @click="close" class="text-4xl text-gray-600 focus:outline-none">
+    <button
+      @click="close"
+      class="text-4xl text-gray-600 focus:outline-none"
+    >
       &times;
     </button>
   </div>
@@ -13,30 +16,54 @@
     <form @submit.prevent="handleSubmit">
       <label class="block">
         <span class="text-lg font-medium text-gray-800">E-mail</span>
-        <input id="email-field" v-model="state.email.value" type="email" :class="{
-          'border-brand-danger': !!state.email.errorMessage
-        }" class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
-          placeholder="jane.dae@gmail.com">
-        <span id="email-error" v-if="!!state.email.errorMessage" class="block font-medium text-brand-danger">
+        <input
+          id="email-field"
+          v-model="state.email.value"
+          type="email"
+          :class="{
+            'border-brand-danger': !!state.email.errorMessage
+          }"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          placeholder="jane.dae@gmail.com"
+        >
+        <span
+          id="email-error"
+          v-if="!!state.email.errorMessage"
+          class="block font-medium text-brand-danger"
+        >
           {{ state.email.errorMessage }}
         </span>
       </label>
 
       <label class="block mt-9">
         <span class="text-lg font-medium text-gray-800">Senha</span>
-        <input id="password-field" v-model="state.password.value" type="password" :class="{
-          'border-brand-danger': !!state.password.errorMessage
-        }" class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
-          placeholder="jane.dae@gmail.com">
-        <span v-if="!!state.password.errorMessage" class="block font-medium text-brand-danger">
+        <input
+          id="password-field"
+          v-model="state.password.value"
+          type="password"
+          :class="{
+            'border-brand-danger': !!state.password.errorMessage
+          }"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          placeholder="jane.dae@gmail.com"
+        >
+        <span
+          v-if="!!state.password.errorMessage"
+          class="block font-medium text-brand-danger"
+        >
           {{ state.password.errorMessage }}
         </span>
       </label>
 
-      <button id="submit-button" :disabled="state.isLoading" type="submit" :class="{
-        'opacity-50': state.isLoading
-      }"
-        class="px-8 py-3 mt-10 text-2xl font-bold text-white transition-all duration-150 rounded-full bg-brand-main focus:outline-none">
+      <button
+        id="submit-button"
+        :disabled="state.isLoading"
+        type="submit"
+        :class="{
+          'opacity-50': state.isLoading
+        }"
+        class="px-8 py-3 mt-10 text-2xl font-bold text-white transition-all duration-150 rounded-full bg-brand-main focus:outline-none"
+      >
         <icon v-if="state.isLoading" name="loading" class="animate-spin" />
         <span v-else>Entrar</span>
       </button>
@@ -45,12 +72,12 @@
 </template>
 
 <script>
-import Icon from '../Icon'
 import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
 import { useField } from 'vee-validate'
 import { useToast } from 'vue-toastification'
 import useModal from '../../hooks/useModal'
+import Icon from '../Icon'
 import { validateEmptyAndLength3, validateEmptyAndEmail } from '../../utils/validators'
 import services from '../../services'
 
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,26 @@
 export default httpClient => ({
+
+  register: async ({ name, email, password }) => {
+    const response = await httpClient.post('auth/register', {
+      name,
+      email,
+      password
+    })
+    let erros = null
+
+    if (!response.data) {
+      erros = {
+        status: response.request.status,
+        statusText: response.request.statusText
+      }
+    }
+
+    return {
+      data: response.data,
+      erros
+    }
+  },
+
   login: async ({ email, password }) => {
     const response = await httpClient.post('auth/login', {
       email,
